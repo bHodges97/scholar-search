929ce0fd7f43db76e34926dab64ef91df00731b6
Benjamin Hodges <benjaminhodges07@gmail.com>
2019-07-30 14:23:17 +0100

    plot

--- analyser.py
+++ analyser.py
@@ -1,20 +1,49 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, strip_accents_unicode
-
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, strip_accents_unicode
+from sklearn.cluster import KMeans,MiniBatchKMeans
+from sklearn.decomposition import TruncatedSVD
+from sklearn import metrics
 from os import walk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords as nltk_stopwords
+from time import time
 import subprocess
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 class Analyser():
-    def analyse(self):
+    def analyse(self,clusters=5,verbose=False):
         stopwords = frozenset(nltk_stopwords.words('english'))#very slow if not set
-        files = [f'downloads/{x}' for x in list(walk('downloads'))[0][2]]
+        filenames = [x for x in list(walk('downloads'))[0][2]]
+        files = [f'downloads/{x}' for x in filenames]
         vectorizer = CountVectorizer(preprocessor=Analyser.preprocess,
                 analyzer="word",
                 tokenizer=Analyser.tokenizer,
-                stop_words=[])
-        x = vectorizer.fit_transform(files)
-        print(vectorizer.get_feature_names())
+                stop_words=[],
+#               max_df=0.5,#ignore top 50%
+                min_df=2,
+                max_features=10000)
+        tf = vectorizer.fit_transform(files)
+        tfidfTransformer = TfidfTransformer()
+        tfidf = tfidfTransformer.fit_transform(tf)
+        svd = TruncatedSVD(n_components=2)
+        reduced = svd.fit_transform(tfidf)
+        #km = MiniBatchKMeans(n_clusters=clusters, init='k-means++', n_init=1, init_size=1000, batch_size=1000, verbose=verbose)
+        km = KMeans(n_clusters=clusters, init='k-means++', max_iter=100, n_init=1, verbose=verbose)
+        km.fit(reduced)
+        centers = km.cluster_centers_
+        y_kmeans = km.predict(reduced)
+        print(y_kmeans)
+        print(y_kmeans.shape,tfidf.shape)
+        print(centers)
+        ax = plt.figure().add_subplot(111)#, projection='3d')
+        ax.scatter(reduced[:, 0], reduced[:, 1], c=y_kmeans,  cmap='viridis')
+        ax.scatter(centers[:, 0], centers[:, 1], c='red', marker='x', alpha=0.5);
+        for i in range(40):
+            ax.annotate(filenames[i],reduced[i])
+        plt.show()
+
+
 
     def preprocess(path):
         text = Analyser.file_to_text(path)
