a135f933974ae3e5c429f3236717e58042c34c1a
Benjamin Hodges <benjaminhodges07@gmail.com>
2019-08-06 13:02:13 +0100

    add cacheing

--- analyser.py
+++ analyser.py
@@ -8,15 +8,15 @@ from nltk.corpus import stopwords as nltk_stopwords
 from time import time
 import subprocess
 
+import scipy.sparse as sp
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 class Analyser():
-    def analyse(self,clusters=5,verbose=False):
-        t0 = time()
+    def count(self, path):
         stopwords = frozenset(nltk_stopwords.words('english'))#very slow if not set
-        filenames = [x for x in list(walk('downloads'))[0][2]]
-        files = [f'downloads/{x}' for x in filenames]
+        filenames = [x for x in list(walk(path))[0][2]]
+        files = [f'{path}/{x}' for x in filenames]
         vectorizer = CountVectorizer(preprocessor=Analyser.preprocess,
                 analyzer="word",
                 tokenizer=Analyser.tokenizer,
@@ -24,26 +24,40 @@ class Analyser():
 #               max_df=0.5,#ignore top 50%
                 min_df=2,
                 max_features=10000)
-        tf = vectorizer.fit_transform(files)
-        t1 = time()
+        self.tf = vectorizer.fit_transform(files)
+        self.tfidf = self.tfidf_transform(self.tf)
+        return self.tfidf
+
+    def tfidf_transform(self,tf):
         tfidfTransformer = TfidfTransformer()
-        tfidf = tfidfTransformer.fit_transform(tf)
+        self.tfidf = tfidfTransformer.fit_transform(tf)
+        return self.tfidf
+
+    def load(self, path):
+        self.tf = sp.load_npz(path)
+        self.tfidf = self.tfidf_transform(self.tf)
+        return self.tfidf
+
+    def save(self, path):
+        sp.save_npz(f"{path}/tfs",self.tfidf)
+
+    def analyse(self, tfidf=None, clusters=5,verbose=False, plot = False):
+        if tfidf == None:
+            tfidf = self.tfidf
         svd = TruncatedSVD(n_components=2)
         reduced = svd.fit_transform(tfidf)
-        t2 = time()
         km = MiniBatchKMeans(n_clusters=clusters, init='k-means++', n_init=1, init_size=1000, batch_size=1000, verbose=verbose)
         #km = KMeans(n_clusters=clusters, init='k-means++', max_iter=100, n_init=1, verbose=verbose)
         km.fit(reduced)
         centers = km.cluster_centers_
         y_kmeans = km.predict(reduced)
-        t3 = time()
-        print(t3-t0,t1-t0,t2-t1,t3-t2)
-        ax = plt.figure().add_subplot(111)#, projection='3d')
-        ax.scatter(reduced[:, 0], reduced[:, 1], c=y_kmeans,  cmap='viridis')
-        ax.scatter(centers[:, 0], centers[:, 1], c='red', marker='x', alpha=0.5);
-        for i in range(40):
-            ax.annotate(filenames[i][3:],reduced[i])
-        plt.show()
+        if plot:
+            ax = plt.figure().add_subplot(111)#, projection='3d')
+            ax.scatter(reduced[:, 0], reduced[:, 1], c=y_kmeans,  cmap='viridis')
+            ax.scatter(centers[:, 0], centers[:, 1], c='red', marker='x')
+            for idx,name in enumerate(filenames):
+                ax.annotate(name[3:],reduced[idx])
+            plt.show()
 
 
 
@@ -73,6 +87,7 @@ class Analyser():
 
 if __name__ == "__main__":
     a = Analyser()
+    X = a.count("downloads")
     a.analyse()
 
 
